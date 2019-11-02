4542abf06984de90b4be44d51c2a6264c43cf03a
lml <soalin228@163.com>
2019-11-02 16:28:00 +0800

    图片组件完成

---
+++ src/components/base/image-view.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="image-view" @click="onClick">
+    <img :src="src"
+         :class="round ? 'round image' : 'image'"
+         :style="{height}"
+         :mode="mode"
+         :lazy-load="lazyLoad"
+         @load="onLoad"
+         @error="onError"
+         v-show="!isLoading && !error"
+    >
+    <img src="https://www.youbaobao.xyz/book/img/loading2.ae9e5924.jpeg"
+         :class="round ? 'round image' : 'image'"
+         :style="{height}"
+         :mode="mode"
+         :lazy-load="lazyLoad"
+         v-show="isLoading || error"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageView',
+  props: {
+    src: {
+      type: String,
+      default: ''
+    },
+    mode: {
+      type: String,
+      default: 'widthFix'
+    },
+    lazyLoad: {
+      type: Boolean,
+      default: true
+    },
+    round: {
+      type: Boolean,
+      default: false
+    },
+    height: {
+      type: String,
+      default: 'auto'
+    }
+  },
+  data () {
+    return {
+      isLoading: true,
+      error: false
+    }
+  },
+  watch: {
+    src (newValue, oldValue) {
+    }
+  },
+  methods: {
+    onClick () {
+      this.$emit('onClick')
+    },
+    onLoad () {
+      this.isLoading = false
+      this.error = false
+    },
+    onError () {
+      this.error = true
+      this.isLoading = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .image-view {
+    width: 100%;
+
+    .image {
+      width: 100%;
+      &.round {
+        border-radius: 50%;
+      }
+    }
+  }
+</style>
