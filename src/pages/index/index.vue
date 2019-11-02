9fa5e32185e84ed19e0ab86ad3166dea126013fe
lml <soalin228@163.com>
2019-11-02 11:00:49 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <span class="msg">hello</span>
+    <van-button type="primary" @click="jump">按钮</van-button>
+  </div>
+</template>
+
+<script>
+import { get } from '@/utils/request'
+
+export default {
+  data () {
+    return {}
+  },
+  methods: {
+    jump () {
+      get('https://www.baidu.com').then(res => {
+        console.log(res)
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .msg {
+    color: red;
+  }
+</style>
