b44dba8e64daf3f89d7bed9314771b5c668f1b49
lml <soalin228@163.com>
2019-11-05 11:03:20 +0800

    search-table组件

---
+++ src/components/search/search-table.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="search-table-wrapper">
+    <div class="search-table-inner">
+      <div class="search-table-book"
+           v-for="(book, index) in data"
+           :key="index"
+           @click="onClick(book)"
+      >
+        <div class="book-img-wrapper">
+          <div class="book-img">
+            <image-view
+              :src="book.cover"></image-view>
+          </div>
+        </div>
+        <div class="book-info-wrapper">
+          <div class="book-title">{{book.title}}</div>
+          <div class="book-author">{{book.author}}</div>
+          <div class="book-category">{{book.category}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import ImageView from '../base/image-view'
+
+export default {
+  name: 'SearchTable',
+  components: {
+    ImageView
+  },
+  props: {
+    data: Array
+  },
+  data () {
+    return {}
+  },
+  methods: {
+    onClick (book) {
+      this.$emit('onClick', book)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .search-table-wrapper {
+    padding: 0 16px;
+
+    .search-table-inner {
+      .search-table-book {
+        display: flex;
+        align-items: center;
+
+        .book-img-wrapper {
+          .book-img {
+            width: 47px;
+            height: 68.5px;
+          }
+        }
+
+        .book-info-wrapper {
+          margin-left: 15.5px;
+          width: 80%;
+
+          .book-title {
+            width: 100%;
+            color: #333;
+            font-size: 16px;
+            line-height: 22.5px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            font-weight: 500;
+          }
+
+          .book-author {
+            color: rgba(0, 0, 0, .65);
+            font-size: 13px;
+            line-height: 18px;
+            margin-top: 5px;
+          }
+
+          .book-category {
+            color: rgba(0, 0, 0, .45);
+            font-size: 13px;
+            line-height: 18px;
+          }
+        }
+      }
+    }
+  }
+</style>
